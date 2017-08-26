481a29924a54dc47f12cdb24e9814cd67f3b0be1
Perruci <pedro.perruci@gmail.com>
2017-08-26 18:37:39 -0300

    Modularized ros calls into functions

--- src/forward_kinematics/forward_kinematics.cpp
+++ src/forward_kinematics/forward_kinematics.cpp
@@ -3,51 +3,68 @@
 #include "std_msgs/Float32.h"
 #include "geometry_msgs/Twist.h"
 
+/* shame on me */
+ros::Publisher right_pub;
+ros::Publisher left_pub;
+
+void moveLinear(float speed)
+{
+    std_msgs::Float32 vel_linear;
+    vel_linear.data = speed;
+    right_pub.publish(vel_linear);
+    left_pub.publish(vel_linear);
+    return;
+}
+void moveAngular(float speed)
+{
+    std_msgs::Float32 vel_spinPlus;
+    std_msgs::Float32 vel_spinMinus;
+    vel_spinPlus.data = speed;
+    vel_spinMinus.data = -speed;
+    right_pub.publish(vel_spinPlus);
+    left_pub.publish(vel_spinMinus);
+    return;
+}
+void moveStop()
+{
+    std_msgs::Float32 vel_stop;
+    vel_stop.data = 0;
+    right_pub.publish(vel_stop);
+    left_pub.publish(vel_stop);
+    return;
+}
+
 int main(int argc, char *argv[])
 {
+    /* ROS setup */
     ros::init(argc, argv, "forward_kinematics");
     // Declare node handle
     ros::NodeHandle n;
     // Topics names
     std::string right = "v_right";
     std::string left = "v_left";
-    // Declare Publishers
-    ros::Publisher right_pub = n.advertise<std_msgs::Float32>(right, 1000);
-    ros::Publisher left_pub = n.advertise<std_msgs::Float32>(left, 1000);
-
+    // Set up global Publishers
+    right_pub = n.advertise<std_msgs::Float32>(right, 1000);
+    left_pub = n.advertise<std_msgs::Float32>(left, 1000);
     ros::Rate loop_rate(100);
     loop_rate.sleep();
-
-    std_msgs::Float32 vel_linear;
-    std_msgs::Float32 vel_spinPlus;
-    std_msgs::Float32 vel_spinMinus;
-    std_msgs::Float32 vel_stop;
-    vel_linear.data = 1.0;
-    vel_spinPlus.data = 1.0;
-    vel_spinMinus.data = -1.0;
-    vel_stop.data = 0;
-
+    /* Program begins */
+    float vel = 0.1;
     char c;
     while(ros::ok())
     {
         std::cin >> c;
         if(c == 'l')
         {
-            right_pub.publish(vel_linear);
-            left_pub.publish(vel_linear);
+            moveLinear(vel);
         }
         if(c == 'r')
         {
-            right_pub.publish(vel_spinPlus);
-            left_pub.publish(vel_spinMinus);
+            moveAngular(vel);
         }
         if(c == 's')
         {
-            right_pub.publish(vel_stop);
-            left_pub.publish(vel_stop);
-            right_pub.publish(vel_stop);
-            left_pub.publish(vel_stop);
-
+            moveStop();
         }
         if(c == 'q')
         {
