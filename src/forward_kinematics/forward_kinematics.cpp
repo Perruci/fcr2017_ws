11cc8c6658d3e9e892f2650d723db7b5e580704c
Perruci <pedro.perruci@gmail.com>
2017-08-28 08:36:58 -0700

    Changed function declaration positions

--- src/forward_kinematics/forward_kinematics.cpp
+++ src/forward_kinematics/forward_kinematics.cpp
@@ -1,33 +1,11 @@
 #include "ros/ros.h"
 #include "lib/ForwardKin.hpp"
 
-void runAndStop(ForwardKin &fKin, float vel, float moveTime)
-{
-    ros::Duration delay(moveTime);
-    fKin.moveLinear(vel);
-    delay.sleep();
-    fKin.moveStop();
-}
+void runAndStop(ForwardKin&, float, float);
 
-double timeTo90deg(float rotationSpeed)
-{
-    const double PI = 3.1415926535897;
-    /* Angular velocity as defined in forward_kinematic_pioneer.cpp */
-    /* (v_right - v_left)/0.5 */
-    float angularVel = 2*rotationSpeed/0.5;
-    double timeToAngle = (PI/2)/angularVel;
-    return timeToAngle;
-}
+double timeTo90deg(float);
 
-void spin90degrees(ForwardKin &fKin, float rotationSpeed)
-{
-    double rotationTime = timeTo90deg(rotationSpeed);
-    /* Set up movement */
-    ros::Duration delay(rotationTime);
-    fKin.moveAngular(rotationSpeed);
-    delay.sleep();
-    fKin.moveStop();
-}
+void spin90degrees(ForwardKin&, float);
 
 void moveSquare(ForwardKin &fKin, float moveSpeed, float timeForward)
 {
@@ -68,4 +46,32 @@ int main(int argc, char *argv[])
         if(c == 'q')
             break;
     }
+}
+
+void runAndStop(ForwardKin &fKin, float vel, float moveTime)
+{
+    ros::Duration delay(moveTime);
+    fKin.moveLinear(vel);
+    delay.sleep();
+    fKin.moveStop();
+}
+
+double timeTo90deg(float rotationSpeed)
+{
+    const double PI = 3.1415926535897;
+    /* Angular velocity as defined in forward_kinematic_pioneer.cpp */
+    /* (v_right - v_left)/0.5 */
+    float angularVel = 2*rotationSpeed/0.5;
+    double timeToAngle = (PI/2)/angularVel;
+    return timeToAngle;
+}
+
+void spin90degrees(ForwardKin &fKin, float rotationSpeed)
+{
+    double rotationTime = timeTo90deg(rotationSpeed);
+    /* Set up movement */
+    ros::Duration delay(rotationTime);
+    fKin.moveAngular(rotationSpeed);
+    delay.sleep();
+    fKin.moveStop();
 }
