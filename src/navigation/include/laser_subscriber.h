954b61771bd0208eb25759a90944e183f2d04404
perruci <pedro.perruci@gmail.com>
2017-09-03 12:23:49 -0300

    Decoding laser messages

---
+++ src/navigation/include/laser_subscriber.h
@@ -0,0 +1,32 @@
+#ifndef LASER_SUB_H
+#define LASER_SUB_H
+
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include <boost/array.hpp>
+
+class LaserSubscriber{
+public:
+    LaserSubscriber();
+    ~LaserSubscriber();
+    ros::NodeHandle nh;
+    ros::Subscriber msg_sub;
+    bool setupComplete;
+    /* Get to know hokuyo_scan */
+    size_t rangesSize;
+    std::vector<float> laserRanges_;
+    float scan_time;        //# time between scans [seconds]
+    float range_min;        //# minimum range value [m]
+    float range_max;        //# maximum range value [m]
+    float angle_min;        //# start angle of the scan [rad]
+    float angle_max;        //# end angle of the scan [rad]
+    float angle_increment;  //# angular distance between measurements [rad]
+    // std::vector<float> laserIntensities_; all intensities return zero...
+
+    void printLaser();
+    void plotResults();
+    void setParameters(const sensor_msgs::LaserScan::ConstPtr&);
+    void laserCallBack(const sensor_msgs::LaserScan::ConstPtr&);
+};
+
+#endif
---
+++ src/navigation/src/laser_subscriber.cpp
@@ -0,0 +1,65 @@
+#include "../include/laser_subscriber.h"
+
+LaserSubscriber::LaserSubscriber()
+{
+    this->setupComplete = false;
+    this->msg_sub = nh.subscribe("/hokuyo_scan", 1000, &LaserSubscriber::laserCallBack, this);
+}
+
+LaserSubscriber::~LaserSubscriber()
+{
+}
+
+void LaserSubscriber::setParameters(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+    this -> scan_time = msg -> scan_time;        //# time between scans [seconds]
+    this -> range_min = msg -> range_min;        //# minimum range value [m]
+    this -> range_max = msg -> range_max;        //# maximum range value [m]
+    this -> angle_min = msg -> angle_min;        //# start angle of the scan [rad]
+    this -> angle_max = msg -> angle_max;        //# end angle of the scan [rad]
+    this -> angle_increment = msg -> angle_increment;  //# angular distance between measurements [rad]
+    this -> rangesSize = sizeof(msg -> ranges);
+    this -> laserRanges_.resize(rangesSize);
+    // this -> laserIntensities_.resize(rangesSize);
+    this -> setupComplete = true;
+}
+
+void LaserSubscriber::laserCallBack(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+    if(!setupComplete)
+        this -> setParameters(msg);
+    else
+        for(size_t i = 0; i < rangesSize; i++)
+        {
+            laserRanges_[i] = msg -> ranges[i];
+            // laserIntensities_[i] = msg -> intensities[i];
+        }
+}
+
+void LaserSubscriber::printLaser()
+{
+    if(setupComplete)
+        for(size_t i = 0; i < rangesSize; i++)
+        {
+            std::cout << i << " Range: " << laserRanges_[i] << '\n';
+            // std::cout << i << " Intensity: " << laserIntensities_[i] << '\n';
+        }
+}
+
+int main(int argc, char *argv[])
+{
+    ros::init(argc, argv, "Laser_subscriber");
+    LaserSubscriber laser_sub;
+
+    ros::Rate loop_rate(25);
+    loop_rate.sleep();
+
+    laser_sub.plotResults();
+
+    while(ros::ok())
+    {
+        laser_sub.printLaser();
+        loop_rate.sleep();
+        ros::spinOnce();
+    }
+}
