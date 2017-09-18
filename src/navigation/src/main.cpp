6a9d0c791a15cfcd3c00bfa39c5f08b8d2fcdf45
Pedro Henrique S. Perruci <pedro.perruci@gmail.com>
2017-09-18 14:34:39 -0300

    New amazing UI

--- src/navigation/src/main.cpp
+++ src/navigation/src/main.cpp
@@ -1,5 +1,7 @@
 #include "../include/navigation.h"
 
+bool startMenu();
+
 int main(int argc, char *argv[])
 {
     Navigation navigate(argc, argv);
@@ -7,7 +9,7 @@ int main(int argc, char *argv[])
 
     geometry_msgs::Point point;
 
-    while(ros::ok())
+    while(ros::ok() && startMenu())
     {
         std::cout << "Where would you like to go? \n"
                   << "Insert X value:\n-> ";
@@ -22,4 +24,30 @@ int main(int argc, char *argv[])
         navigate.stopMoving();
     }
     return 0;
+}
+
+bool startMenu()
+{
+    std::cout << "Hello Human o/" << '\n';
+    std::cout << "What would you like me to do?" << '\n';
+    std::cout << "\tm --- move to a point" << '\n';
+    std::cout << "\tq --- quit and sleep" << '\n';
+    std::cout << "-> ";
+    while (true)
+    {
+        char c;
+        std::cin >> c;
+        switch (c)
+        {
+            case 'm':
+                return true;
+                break;
+            case 'q':
+                return false;
+                break;
+            default:
+                std::cout << "Oops, I didn't get it\ntry again: -> " << '\n';
+                break;
+        }
+    }
 }
