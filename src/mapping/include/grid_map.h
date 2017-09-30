2ce1b14e14a6b6824b1ceaffd19a6e52bb05d27c
Pedro Henrique S. Perruci <pedro.perruci@gmail.com>
2017-09-30 15:18:11 -0300

    Grid map updates when reciving laser scan messages

--- src/mapping/include/grid_map.h
+++ src/mapping/include/grid_map.h
@@ -4,6 +4,7 @@
 #include <ros/ros.h>
 #include <grid_map_ros/grid_map_ros.hpp>
 #include <grid_map_msgs/GridMap.h>
+#include <sensor_msgs/LaserScan.h>
 #include <cmath>
 
 namespace grid_map_params
@@ -21,15 +22,17 @@ public:
 
     void laserCallBack();
     void createGridMap();
-    void generateGridMap(ros::Time& time);
+    void generateGridMap();
     void publishGridMap(ros::Time& time);
 
     inline bool ok(){return nh_.ok();};
 
 private:
+    void laserCallBack(const sensor_msgs::LaserScan::ConstPtr&);
+
     ros::NodeHandle nh_;
     ros::Publisher publisher;
-    // ros::Subscriber subscriber;
+    ros::Subscriber subscriber;
     grid_map::GridMap map;
 };
 
--- src/mapping/src/grid_map_node.cpp
+++ src/mapping/src/grid_map_node.cpp
@@ -4,13 +4,18 @@ Grid_Mapping::Grid_Mapping()
 {
     // Initialize node and publisher.
     this->publisher = nh_.advertise<grid_map_msgs::GridMap>("grid_map_node/grid_map", 1, true);
-    // this->subscriber = nh_.subscribe("/hokuyo_scan", 1000, callback);
+    this->subscriber = nh_.subscribe("/hokuyo_scan", 1000, &Grid_Mapping::laserCallBack, this);
     this->createGridMap();
 }
 
 Grid_Mapping::~Grid_Mapping()
 {
 
+}
+
+void Grid_Mapping::laserCallBack(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+    this->generateGridMap();
 }
 
 void Grid_Mapping::createGridMap()
@@ -26,19 +31,16 @@ void Grid_Mapping::createGridMap()
               map.getSize()(0), map.getSize()(1));
 }
 
-// void Grid_Mapping::laserCallBack(const sensor_msgs::LaserScan::ConstPtr& msg)
-// {
-//
-// }
-
-void Grid_Mapping::generateGridMap(ros::Time& time)
+void Grid_Mapping::generateGridMap()
 {
+    ros::Time time = ros::Time::now();
     for (grid_map::GridMapIterator it(this->map); !it.isPastEnd(); ++it)
     {
         grid_map::Position position;
         map.getPosition(*it, position);
         map.at("obstacles", *it) = -0.04 + 0.2 * std::sin(3.0 * time.toSec() + 5.0 * position.y()) * position.x();
     }
+    this->publishGridMap(time);
 }
 
 void Grid_Mapping::publishGridMap(ros::Time& time)
@@ -56,16 +58,10 @@ int main(int argc, char** argv)
     // Initialize node and publisher.
     ros::init(argc, argv, "grid_map_node");
     Grid_Mapping gmap;
-    // Work with grid map in a loop.
-    ros::Rate rate(30.0);
+    gmap.generateGridMap();
+    /* Wait for Laser data to generate grid map */
     while (gmap.ok())
     {
-        // Add data to grid map.
-        ros::Time time = ros::Time::now();
-        gmap.generateGridMap(time);
-        gmap.publishGridMap(time);
-        // Wait for next cycle.
-        rate.sleep();
         ros::spinOnce();
     }
     return 0;
