b66f36bfc8cfaca36b493830b424ad61a2d971f4
Pedro Henrique S. Perruci <pedro.perruci@gmail.com>
2017-10-03 23:50:35 -0300

    Skeleton of topological mapping

---
+++ src/mapping/scripts/dijkstra.py
@@ -0,0 +1,55 @@
+from graph import Graph
+from cic_map import getMap
+from collections import defaultdict, deque
+
+def dijkstra(graph, initial):
+    visited = {initial: 0}
+    path = {}
+
+    nodes = set(graph.nodes)
+
+    while nodes:
+        min_node = None
+        for node in nodes:
+            if node in visited:
+                if min_node is None:
+                    min_node = node
+                elif visited[node] < visited[min_node]:
+                    min_node = node
+        if min_node is None:
+            break
+
+        nodes.remove(min_node)
+        current_weight = visited[min_node]
+
+        for edge in graph.edges[min_node]:
+            try:
+                weight = current_weight + graph.distances[(min_node, edge)]
+            except:
+                continue
+            if edge not in visited or weight < visited[edge]:
+                visited[edge] = weight
+                path[edge] = min_node
+
+    return visited, path
+
+
+def shortest_path(graph, origin, destination):
+    visited, paths = dijkstra(graph, origin)
+    full_path = deque()
+    _destination = paths[destination]
+
+    while _destination != origin:
+        full_path.appendleft(_destination)
+        _destination = paths[_destination]
+
+    full_path.appendleft(origin)
+    full_path.append(destination)
+
+    return visited[destination], list(full_path)
+
+if __name__ == '__main__':
+
+    graph = getMap()
+
+print(shortest_path(graph, 'A', 'D')) # output: (25, ['A', 'B', 'D'])
---
+++ src/mapping/scripts/graph.py
@@ -0,0 +1,19 @@
+import rospy
+from geometry_msgs.msg import Point
+from collections import defaultdict, deque
+
+# inspired by https://gist.github.com/mdsrosa/c71339cb23bc51e711d8
+
+class Graph(object):
+    def __init__(self):
+        self.nodes = set()
+        self.edges = defaultdict(list)
+        self.distances = {}
+
+    def add_node(self, value):
+        self.nodes.add(value)
+
+    def add_edge(self, from_node, to_node, distance):
+        self.edges[from_node].append(to_node)
+        self.edges[to_node].append(from_node)
+        self.distances[(from_node, to_node)] = distance
