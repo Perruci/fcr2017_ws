e7e0f5c27253c8ff94ef9d39494d676371a83c75
Pedro Henrique S. Perruci <pedro.perruci@gmail.com>
2017-10-09 23:41:43 -0300

    Implemented Layer subclass

--- src/mapping/src/grid_map/grid_map.py
+++ src/mapping/src/grid_map/grid_map.py
@@ -3,8 +3,10 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-class GridMap:
-    def __init__(self):
+class Layer:
+
+    def __init__(self, name):
+        # basic setup
         self.grid_widith = 25
         self.grid_height = 20
         # number of points per meter
@@ -12,22 +14,40 @@ class GridMap:
         # get rows and cols
         self.rows = self.grid_height * self.resolution
         self.cols = self.grid_widith * self.resolution
+        self.name = name
         # create empty numpy instace
         self.grid = np.ones([self.rows, self.cols], np.float32)/2
 
-    def print_grid_params(self):
-        print 'Grid Created'
-        print '\t widith: ',self.grid_widith,'m, height: ',self.grid_height,'m, resolution: ',self.resolution
-        print '\t grid shape: ',self.grid.shape
+    def get_grid(self):
+        return self.grid
 
-    def show_grid_map(self, windowname):
-        cv2.imshow(windowname, self.grid)
+class GridMap:
+    def __init__(self):
+        self.layer_dict = {}
+        self.num_layer = 0
+
+    def __iter__(self):
+        return iter(self.layer_dict.values())
+
+    def add_layer(self, node):
+        self.num_layer = self.num_layer + 1
+        new_layer = Layer(node)
+        self.layer_dict[node] = new_layer
+
+    def get_grid(self, node):
+        if node in self.layer_dict:
+            return self.layer_dict[node].get_grid()
+        else:
+            return None
+
+    def show_grid_map(self, node, windowname):
+        cv2.imshow(windowname, self.get_grid(node))
         cv2.waitKey(0)
 
-    def save_grid_map(self, filename):
-        mat = cv2.cvtColor(self.grid, cv2.COLOR_GRAY2BGR)
-        cv2.imwrite(filename, mat)
+    def save_grid_map(self, node, filename):
+        cv2.imwrite(filename, self.get_grid(node))
 
     def run(self):
-        self.print_grid_params()
-        self.show_grid_map('grid_map')
+        self.add_layer('1')
+        self.show_grid_map('1', 'grid_map')
+        self.save_grid_map('1', 'grid.png')
