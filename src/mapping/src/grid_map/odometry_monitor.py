3e79e00ceacae674a67456ddd173a968592dfd99
Pedro Henrique S. Perruci <pedro.perruci@gmail.com>
2017-11-01 14:59:22 -0200

    Safety measures on odometry monitor

--- src/mapping/src/grid_map/odometry_monitor.py
+++ src/mapping/src/grid_map/odometry_monitor.py
@@ -6,10 +6,17 @@ class OdometryMonitor:
     def odometry_callback(self, odometry_data):
         ''' get pose from odometry message '''
         self.pose = odometry_data.pose.pose
+        if self.first_run:
+            self.first_run = False
 
     def __init__(self):
         self.sub_odometry = rospy.Subscriber('pose', Odometry, self.odometry_callback)
+        self.first_run = True
 
 
     def get_position(self):
-        return self.pose.position
+        if self.first_run:
+            print 'Waiting for odometry message'
+            return None
+        else:
+            return self.pose.position
