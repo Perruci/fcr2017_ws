8e0f996cc42ad915ced7113e5ce53b301eafba4b
Pedro Henrique S. Perruci <pedro.perruci@gmail.com>
2017-11-06 11:35:49 -0200

    Line length as a parameter for printing

--- src/topological_localization/src/line_features/line_features.py
+++ src/topological_localization/src/line_features/line_features.py
@@ -1,5 +1,13 @@
 import rospy
+import math
 from topological_localization.msg import LineSegmentList
+
+def line_length(start, end):
+    ''' Compute line length through the pitagorean theorem '''
+    diff_x = start[0] - end[0]
+    diff_y = start[1] - end[1]
+
+    return math.sqrt(diff_x**2 + diff_y**2)
 
 class LineFeatures:
     def line_extraction_callback(self, msg):
@@ -9,16 +17,19 @@ class LineFeatures:
         self.line_list = []
         self.sub_lines = rospy.Subscriber('line_segments', LineSegmentList, self.line_extraction_callback)
 
+    def print_line(self, line):
+        print 'New line'
+        print '\trho = ', line.radius
+        print '\tangle = ', line.angle
+        print '\tstart = ', line.start[0], line.start[1]
+        print '\tend = ', line.end[0], line.end[1]
+        print '\tlength = ', line_length(line.start, line.end)
+
     def print_lines(self):
         print 'Lines avaliable:'
         if self.line_list is not None:
-            for line in self.line_list:
-                print 'New line'
-                print '\trho = ', line.radius
-                print '\tangle = ', line.angle
-                print '\tstart = ', line.start[0], line.start[1]
-                print '\tend = ', line.end[0], line.end[1]
-        print 'Next line'
+            map(self.print_line, self.line_list)
+        print 'Next iteration'
 
     def run(self):
         self.print_lines()
