d2583335b53442753cd6c5d79363b7cf22e4b57d
Pedro Henrique S. Perruci <pedro.perruci@gmail.com>
2017-11-12 18:18:31 -0200

    Message prototipe printed to screen. Parallel lines parameters updated

--- src/topological_localization/src/line_features/line_features.py
+++ src/topological_localization/src/line_features/line_features.py
@@ -15,18 +15,25 @@ def wrap_angle(phases):
     wrap = ( phases + np.pi) % (2 * np.pi ) - np.pi
     return wrap
 
-
-
 class LineFeatures:
+    ''' Class designed to process line_segments topic and extract line features from it. '''
     def line_extraction_callback(self, msg):
+        ''' Capture line_segments messages and process their data '''
         self.line_list = np.array(msg.line_segments)
+        self.process_line()
 
     def __init__(self):
+        ''' Line features constructor. Create ROS publisher and subscriber ans set default valued variables '''
         self.line_list = []
         self.sub_lines = rospy.Subscriber('line_segments', LineSegmentList, self.line_extraction_callback)
+        # tolerance values
         self.parallel_min_dist = 1.5
-        self.parallel_max_dist = 5
+        self.parallel_max_dist = 10
         self.continuity_tolerance = 1
+        # message variables
+        self.found_hallway = False
+        self.found_corner = False
+        self.found_mult_corner = False
 
     def  get_parallel_and_orthogonal(self, tolerance):
         ''' Extract line properties as parallel and orthogonal lines '''
@@ -58,7 +65,7 @@ class LineFeatures:
 
         dist = abs(line1.radius - line2.radius)
         if self.parallel_min_dist < dist and dist < self.parallel_max_dist:
-            print 'parallel lines distance: ', dist
+            # print 'parallel lines distance: ', dist
             return True
         else:
             return False
@@ -72,6 +79,7 @@ class LineFeatures:
         return parallel_pairs
 
     def tuple_inside_tolerance(self, tup):
+        ''' Veify continuity tolerance on orthogonal line tuples '''
         if tup[0] < self.continuity_tolerance and tup[1] < self.continuity_tolerance:
             return True
         else:
@@ -100,7 +108,6 @@ class LineFeatures:
         orthogonal_pairs = filter(self.orthogonal_validation, orthogonal_pairs)
         return orthogonal_pairs
 
-
     def process_line(self, tolerance=0.01):
         ''' Process resulting lines to classify their shape '''
         parallel_pairs, orthogonal_pairs = self.get_parallel_and_orthogonal(tolerance)
@@ -108,8 +115,41 @@ class LineFeatures:
         parallel_pairs = self.filter_parallels(parallel_pairs)
         orthogonal_pairs = self.filter_orthogonals(orthogonal_pairs)
 
-        print parallel_pairs
-        print orthogonal_pairs
+        # print 'Paralel lines found:', len(parallel_pairs), parallel_pairs
+        # print 'Orthogonal lines found:', len(orthogonal_pairs), orthogonal_pairs
+        self.update_message(parallel_pairs,orthogonal_pairs)
+
+
+    def update_message(self, parallel_pairs, orthogonal_pairs):
+        '''
+            Update variables self.found_hallway and self.found_corner which composes the message.
+            These variables are reset after the publish
+        '''
+        if (not self.found_hallway) and len(parallel_pairs) > 0:
+            self.found_hallway = True
+
+        if (not self.found_corner) and len(orthogonal_pairs) > 0:
+            self.found_corner = True
+
+        if (not self.found_mult_corner) and len(orthogonal_pairs) > 1:
+            self.found_mult_corner = True
+
+    def publish_message(self):
+        msg = np.zeros(3)
+        if self.found_hallway:
+            msg[0] = 1
+        if self.found_corner and not self.found_mult_corner:
+            msg[1] = 1
+            msg[2] = 1
+        elif self.found_mult_corner:
+            msg[1] = 2
+            msg[2] = 2
+        print 'Message published!', msg
+
+        self.found_hallway, self.found_corner, self.found_mult_corner = False, False, False
+
+    def run(self):
+        self.publish_message()
 
     def print_line(self, line):
         print 'New line'
@@ -124,7 +164,3 @@ class LineFeatures:
         if self.line_list is not None:
             map(self.print_line, self.line_list)
         print 'Next iteration'
-
-    def run(self):
-        self.process_line()
-        self.print_lines()
